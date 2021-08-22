764f8d19e8a94672dfbc719909a309036bd37543
Grady Congdon <gradycongdon@gmail.com>
2021-08-21 21:09:08 -0400

    Name+ then year+

--- src/Sorting.tsx
+++ src/Sorting.tsx
@@ -4,7 +4,7 @@ import { TeaProduct } from "./TeaProduct";
 const getName = (x: TeaProduct) => x.name.replace('The ', '')
 
 const nameDesc = (a: TeaProduct, b: TeaProduct) => getName(b).localeCompare(getName(a)) || b.year - a.year;
-const nameAsc = (a: TeaProduct, b: TeaProduct) => getName(a).localeCompare(getName(b)) || b.year - a.year;
+const nameAsc = (a: TeaProduct, b: TeaProduct) => getName(a).localeCompare(getName(b)) || a.year - b.year;
 
 const yearDesc = (a: TeaProduct, b: TeaProduct) => b.year - a.year || nameAsc(a, b);
 const yearAsc = (a: TeaProduct, b: TeaProduct) => a.year - b.year || nameAsc(a, b);
